0268845ddb124fe1e6f1987027ac719743075952
Danny Scott <dannyrscott@gmail.com>
2013-06-12 08:56:18 -0400

    initial commit

---
+++ dist/BingMapFFDraw.js
@@ -0,0 +1,13 @@
+/*! BingMapFFDraw - v0.0.1 - 2013-06-12
+* https://github.com/dannyrscott/BingMapFreeFormDraw
+* Copyright (c) 2013 Danny Scott; Licensed MIT */
+(function(exports) {
+
+  'use strict';
+
+  var BingDrawFF = function() {
+
+  };
+
+  exports.BingDrawFF = BingDrawFF;
+}(typeof exports === 'object' && exports || this));
